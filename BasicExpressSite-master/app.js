6f6afde288a21ced3149407703dd81419351c32b
Antony Canut <antony.canut@studentpartner.com>
2016-04-22 14:28:01 +0200

    Modifications mineures

--- BasicExpressSite-master/app.js
+++ BasicExpressSite-master/app.js
@@ -6,8 +6,12 @@ var express = require('express')
   , nib = require('nib')
 
 var logger = require('morgan');
+var bodyParser = require('body-parser')
+
+// Routing
 var routes = require('./routes/index');
 var about = require('./routes/about');
+var contact = require('./routes/contact');
 
 var app = express();
 
@@ -15,26 +19,26 @@ function compile(str, path) {
   return stylus(str)
     .set('filename', path)
     .use(nib());
-}
+};
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
-app.use(logger('dev'))
+app.use(logger('dev'));
 app.use(stylus.middleware(
   { src: __dirname + '/public'
   , compile: compile
   }
-))
+));
 app.use(express.static(__dirname + '/public'));
+app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+}));
 
+
+// Routing
 app.use('/', routes);
 app.use('/about', about);
-
-//
-// app.get('/', function (req, res) {
-//   res.render('index',
-//   { title : 'Home' }
-//   )
-// })
+app.use('/contact', contact);
 
 app.listen(3000);
--- BasicExpressSite-master/routes/contact.js
+++ BasicExpressSite-master/routes/contact.js
@@ -15,5 +15,14 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('contact');
 });
+router.post('/', function(req, res, next){
+  res.render('contact', {
+    Name: req.body.name,
+    FirstName: req.body.firstName,
+    Mail: req.body.mail,
+    Message: req.body.message
+  })
+  // res.receiver.postMessage('Hello Treehouse!', 'http://demos.mattwest.io');
+});
 
 module.exports = router;
